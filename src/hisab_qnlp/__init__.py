ace98569da022ef3aa05e24d608b3bedf8464d92
Nizarimrani <nizar.el.imrani@gmail.com>
2026-09-19 11:23:54 +0100

    Add reproducibility package

---
+++ src/hisab_qnlp/__init__.py
@@ -0,0 +1,6 @@
+"""Hisab Labs compositional quantum-semantics reference package."""
+
+from .composition import ExperimentConfig, build_circuit, run_experiment, summarize_counts
+
+__all__ = ["ExperimentConfig", "build_circuit", "run_experiment", "summarize_counts"]
+
